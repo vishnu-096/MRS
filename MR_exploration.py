67a9d08582d8d2599dccdadce703eaa9d230d6fe
affanbinusman <affanafo@gmail.com>
2022-12-03 23:51:38 -0700

    Refinement of last version - no major change

--- MR_exploration.py
+++ MR_exploration.py
@@ -14,7 +14,7 @@ minmaxrbt1 = [[xmin1, ymin1], [xmin1, ymax1], [xmax1, ymin1], [xmax1, ymax1]]
 minmaxrbt2 = [[xmin2, ymin2], [xmin2, ymax2], [xmax2, ymin2], [xmax2, ymax2]]
 minmax = [minmaxrbt1, minmaxrbt2]
 
-
+################################
 
 # Functions
 # Initializations of robots
@@ -26,10 +26,8 @@ def multiple_robots(no_of_robots):
     return list1 
     
 def init_robot():
-    # x = random.randint(8,8)
     x = 10
     y = random.randint(5,85)
-    # x, y = 5,95
     rbt = robot([x,y],1,1)
     rbt.get_sensor_readings_and_update()
     return rbt
@@ -45,27 +43,11 @@ max_counter = 100
 def explore_frontier_random(rbt):
     return random_index(rbt)
 
-# Working copy of explore_frontier_far(rbt):
-# def explore_frontier_far(rbt):
-#     counter = 0
-#     while True:
-#         x = random_index(rbt)
-#         distance = euc_dist(rbt, x)
-#         if distance >= 25:
-#             print("Max counter NOT triggered")
-#             return x
-#         counter += 1
-#         if counter >= max_counter:
-#             print("Max counter triggered")
-#             return explore_frontier_random(rbt)
-
 def explore_frontier_far(rbt, allrobots, minmax):
     listofindexforpoints = list()
     numberofpoints = 5
     costlist = defaultdict()
-    # costlist = [[0]] * numberofpoints
-
-    
+        
     for _ in range(numberofpoints):
         counter = 0
         check = True
@@ -91,26 +73,17 @@ def explore_frontier_far(rbt, allrobots, minmax):
                 minlistpointindex = list()
 
                 for j in range(4):
-                    # Fix x issue in this case - pass x
                     minlist.append(euc_dist(val, x=listofindexforpoints[r] , minmax=minmax[i][j]))
                     minlistpointindex.append(listofindexforpoints[r])
                 
                 min_in_minlist = min(minlist)
                 costlist[min_in_minlist] = minlistpointindex[minlist.index(min_in_minlist)]
                 
-                # costlist[min(minlist)] = minlist.index(min(minlist))
-                # costlist.append( [min(minlist), minlist.index(min(minlist))] )
-
             print("costlist", costlist)
             
             maxdistancevalue = max(costlist.keys())
             print(costlist[maxdistancevalue])
             return costlist[maxdistancevalue]
-
-            # max_check = -100
-            # for i in range(numberofpoints):
-            #     max_check = max(max_check , costlist[i][0])
-
 
 def explore_frontier_closeby(rbt):
     counter = 0
@@ -134,16 +107,11 @@ def euc_dist(rbt1, x=None, rbt2=None, minmax=None):
     if rbt2 is None and minmax is None:
         return math.sqrt(((rbt1.map.frontiers[x][0] - rbt1.cur_pos[0]) ** 2 ) + ((rbt1.map.frontiers[x][1] - rbt1.cur_pos[1]) ** 2 ))
     elif minmax is not None:
-        # print("rbt1", rbt1.map.frontiers[x][0])
-        # print("rbt2", rbt1.map.frontiers[x][1])
-        # print(minmax[0])
-        # print(minmax[1])
         return math.sqrt(((rbt1.map.frontiers[x][0] - minmax[0]) ** 2 ) + ((rbt1.map.frontiers[x][1] - minmax[1]) ** 2 ))
     else:
-        a = 0
         ################# Yet to be implemented
         print("THIS FUNC IS NOT COMPLETE")
-        return a
+        return False
 
 # Robot actions
 def move_to_goal(rbt, x):
@@ -165,13 +133,12 @@ def move_to_goal(rbt, x):
 robots = multiple_robots(2)
 
 # # First movement of robots
-for _ in range(3):
+for _ in range(2):
     for r in range(len(robots)):
         move_to_goal(robots[r], explore_frontier_random(robots[r]))
 
 # # Exploration 
 i, p = 0, 0
-# while i < 100:
 while i < 100:
     for r in range(len(robots)):
         if p == 0:
@@ -186,9 +153,8 @@ while i < 100:
         i += 1
         
         # condition for fast wider exploration in the beginning
-        # p = random.randint(0,1)       # For testing purposes
         if i > 10:
-            p = random.randint(0,3)
+            p = random.randint(0,2)
         else:
             p = random.randint(0,1)
 
