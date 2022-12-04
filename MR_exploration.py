06d06c0f4c94693d9f914cddc026bfacf658054b
affanbinusman <affanafo@gmail.com>
2022-12-04 12:24:49 -0700

    Cost func implemented. Works for any no of robots

--- MR_exploration.py
+++ MR_exploration.py
@@ -12,7 +12,12 @@ ymax1, ymax2 = 10, 95
 
 minmaxrbt1 = [[xmin1, ymin1], [xmin1, ymax1], [xmax1, ymin1], [xmax1, ymax1]]
 minmaxrbt2 = [[xmin2, ymin2], [xmin2, ymax2], [xmax2, ymin2], [xmax2, ymax2]]
-minmax = [minmaxrbt1, minmaxrbt2]
+# For 2 robots
+# minmax = [minmaxrbt1, minmaxrbt2]
+# For 3 robots
+minmax = [minmaxrbt1, minmaxrbt2, minmaxrbt2 ]
+# For 5 robots
+# minmax = [minmaxrbt1, minmaxrbt2, minmaxrbt1, minmaxrbt2, minmaxrbt2 ]
 
 ################################
 
@@ -26,8 +31,10 @@ def multiple_robots(no_of_robots):
     return list1 
     
 def init_robot():
+    # x = random.randint(8,8)
     x = 10
     y = random.randint(5,85)
+    # x, y = 5,95
     rbt = robot([x,y],1,1)
     rbt.get_sensor_readings_and_update()
     return rbt
@@ -47,6 +54,7 @@ def explore_frontier_far(rbt, allrobots, minmax):
     listofindexforpoints = list()
     numberofpoints = 5
     costlist = defaultdict()
+    costfunctiondict = defaultdict()
         
     for _ in range(numberofpoints):
         counter = 0
@@ -61,29 +69,26 @@ def explore_frontier_far(rbt, allrobots, minmax):
             if counter >= max_counter:
                 listofindexforpoints.append(explore_frontier_random(rbt)) 
                 check = False
-
-    print("(listofindexforpoints):" ,listofindexforpoints)
-    print("listofindexforpoints[0]:" ,listofindexforpoints[0])
+    
     for i, val in enumerate(allrobots):
         if val == rbt:
             continue
         else:
             for r in range(numberofpoints):
                 minlist = list()
-                minlistpointindex = list()
 
                 for j in range(4):
-                    minlist.append(euc_dist(val, x=listofindexforpoints[r] , minmax=minmax[i][j]))
-                    minlistpointindex.append(listofindexforpoints[r])
+                    minlist.append(euc_dist(rbt, x=listofindexforpoints[r] , minmax=minmax[i][j]))
                 
                 min_in_minlist = min(minlist)
-                costlist[min_in_minlist] = minlistpointindex[minlist.index(min_in_minlist)]
-                
-            print("costlist", costlist)
-            
-            maxdistancevalue = max(costlist.keys())
-            print(costlist[maxdistancevalue])
-            return costlist[maxdistancevalue]
+                costlist[listofindexforpoints[r]] = min_in_minlist
+                try:
+                    costlist[listofindexforpoints[r]] += min_in_minlist
+                except Exception:
+                    costlist[listofindexforpoints[r]] = min_in_minlist
+
+    maxdistancevalue = max(costlist.values())
+    return (list(costlist.values())).index(maxdistancevalue)
 
 def explore_frontier_closeby(rbt):
     counter = 0
@@ -91,7 +96,6 @@ def explore_frontier_closeby(rbt):
         x = random_index(rbt)
         distance = euc_dist(rbt, x)
         if distance <= 7:
-            print("Max counter NOT triggered")
             return x
         counter += 1
         if counter >= max_counter:
@@ -109,9 +113,10 @@ def euc_dist(rbt1, x=None, rbt2=None, minmax=None):
     elif minmax is not None:
         return math.sqrt(((rbt1.map.frontiers[x][0] - minmax[0]) ** 2 ) + ((rbt1.map.frontiers[x][1] - minmax[1]) ** 2 ))
     else:
-        ################# Yet to be implemented
-        print("THIS FUNC IS NOT COMPLETE")
-        return False
+        a = 0
+        ################# Not implemented / Not required #################
+        print("Triggered wrong condition in euc_dist")
+        return a
 
 # Robot actions
 def move_to_goal(rbt, x):
@@ -130,19 +135,24 @@ def move_to_goal(rbt, x):
 ########################################
 
 # Initializing number of robots
-robots = multiple_robots(2)
+robots = multiple_robots(3)
 
 # # First movement of robots
 for _ in range(2):
     for r in range(len(robots)):
         move_to_goal(robots[r], explore_frontier_random(robots[r]))
 
+for r in range(len(robots)):
+    plt.subplot(1,len(robots),r+1) 
+    robots[r].map.display_robot_map()
+plt.savefig("Initialpoints.png")
+# plt.show()
+
 # # Exploration 
 i, p = 0, 0
 while i < 100:
     for r in range(len(robots)):
         if p == 0:
-            # move_to_goal(robots[r], explore_frontier_far(robots[r]))   
             try:
                 move_to_goal(robots[r], explore_frontier_far(robots[r], robots, minmax))   
             except Exception:
@@ -153,13 +163,15 @@ while i < 100:
         i += 1
         
         # condition for fast wider exploration in the beginning
+        # p = random.randint(0,1)       # For testing purposes
         if i > 10:
-            p = random.randint(0,2)
+            p = random.randint(0,4)
         else:
             p = random.randint(0,1)
 
 # Displaying explored areas
 for r in range(len(robots)):
-    plt.subplot(1,2,r+1) 
+    plt.subplot(1,len(robots),r+1) 
     robots[r].map.display_robot_map()
-plt.show()
+plt.savefig("Finalpoints.png")
+# plt.show()
